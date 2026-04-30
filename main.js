9b9c7a3f1870e3373650b6e69a46dc56b42b9332
OFamosoCientista <vinicius.maxboy22@gmail.com>
2026-04-30 17:31:33 -0300

    0.0.0

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ onAuthStateChanged(auth, (user) => {
 });
 
 // 🔹 LOGOUT
-function fazerLogout() {
+window.fazerLogout = function () {
   signOut(auth)
     .then(() => {
       alert("Saiu da conta");
